7826d3d719ec18698c2648d759bd2464550885cd
Rahul Dutta <drahul2@gmail.com>
2013-07-14 02:21:31 +0000

    added logic to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs  = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var data = fs.readFileSync(this.dir+"/index.html");
+  var buf = new Buffer(256);
+  buf.write(data,0);
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
